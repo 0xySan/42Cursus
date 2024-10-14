f87bc4282f3df254e90da5bb7e6c95c48ee8d243
0xysan <ethtaquet@gmail.com>
2024-10-14 22:05:22 +0200

    corrected split ig?

--- libft.a/ft_split.c
+++ libft.a/ft_split.c
@@ -13,65 +13,95 @@
 #include "libft.h"
 #include <stdio.h>
 
-int is_delimiter(char str, char c)
+int count_words(const char *s, char c) 
 {
-	return (str == c || str == '\0');
+    int count;
+	int in_word;
+
+	count = 0;
+	in_word= 0;
+    while (*s) 
+    {
+        if (*s != c)
+        {
+            if (!in_word) 
+            {
+                in_word = 1;
+                count++;
+            }
+        } 
+        else 
+            in_word = 0;
+        s++;
+    }
+    return count;
 }
 
-int count_words(char const *str, char c)
+char *extract_word(const char *s, int *index, char c) 
 {
-	int count = 0;
-	int in_word = 0;
-
-	while (*str)
-	{
-		if (!is_delimiter(*str, c) && !in_word++)
-			count++;
-		else
-			in_word = 0;
-		str++;
-	}
-	return (count);
-}
-
-char *copy_word(char const *str, char c)
-{
+    int start;
 	int len;
-	int i;
+	char *word;
 
-	len = -1;
-	while (!is_delimiter(str[++len], c));
-	char *word = (char *)malloc((len + 1) * sizeof(char));
-	if (!word)
-		return NULL;
-	i = -1;
-	while (++i < len)
-		word[i] = str[i];
-	word[i] = '\0';
-	return (word);
+	start = *index;
+
+    while (s[*index] && s[*index] !=c) 
+        (*index)++;
+	len = *index - start;
+	word = malloc(len + 1);
+    if (word) 
+    {
+        ft_strncpy(word, s + start, len);
+        word[len] = '\0';
+    }
+    return word;
 }
 
-char **ft_split(char const *str, char c)
+void split_words(char const *s, char c, char **result) 
+{
+    int index;
+	int word_index;
+
+	index = 0;
+	word_index = 0;
+    while (s[index]) 
+    {
+        if (s[index] != c) 
+            result[word_index++] = extract_word(s, &index, c);
+        else 
+            index++;
+    }
+}
+
+char **ft_split(char const *s, char c) 
 {
 	int word_count;
-	int index;
 	char **result;
 
-	word_count = count_words(str, c);
-	result = (char **)malloc((word_count + 1) * sizeof(char *));
-	if (!result)
-		return NULL;
-	index = 0;
-	str--;
-	while (*++str)
-	{
-		if (!is_delimiter(*str, c))
-		{
-			result[index++] = copy_word(str, c);
-			while (*str && !is_delimiter(*str, c))
-				str++;
-		}
-	}
-	result[index] = NULL;
-	return (result);
+    if (!s) 
+        return NULL;
+	word_count = count_words(s, c);
+	result = malloc((word_count + 1) * sizeof(char *));
+    if (!result) 
+        return NULL;
+    split_words(s, c, result);
+    return result;
+}
+
+int main() 
+{
+    char **result;
+    const char *s = "Hello, world! This is a test.";
+    char delimiter = ' ';
+
+    result = ft_split(s, delimiter);
+
+    for (int i = 0; result && result[i]; i++) 
+    {
+        printf("Word %d: %s\n", i + 1, result[i]);
+        free(result[i]); // Free each word
+    }
+    free(result); // Free the array
+
+    return 0;
 }

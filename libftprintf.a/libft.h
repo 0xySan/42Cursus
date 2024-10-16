61846eaa033306a182aac9dcd8973182afe36721
0xySan <ethtaquet@gmail.com>
2024-10-16 14:46:12 +0200

    did all the norm thingy again

--- libftprintf.a/libft.h
+++ libftprintf.a/libft.h
@@ -12,22 +12,21 @@
 
 #ifndef LIBFT_H
 # define LIBFT_H
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdarg.h>
 
 # define INT_MAX 2147483647
 # define INT_MIN -2147483648
-typedef unsigned long size_t;
-void ft_putstr(char *str);
-void ft_putchar(int c);
-int	ft_count_nb(int nb, int len);
-void	ft_utoa(unsigned int nb);
-int	ft_count_unsigned(unsigned int nb);
-void    ft_ultoa(unsigned long nbr, const char *base);
-void    ft_ltoa(long nbr, const char *base);
-void    ft_ptoa(void *ptr);
 
+void	ft_putstr(char *str);
+void	ft_putchar(int c);
+int		ft_count_nb(int nb, int len);
+void	ft_utoa(unsigned int nb);
+int		ft_count_unsigned(unsigned int nb);
+void	ft_ultoa(unsigned long nbr, const char *base);
+void	ft_ltoa(long nbr, const char *base);
+void	ft_ptoa(void *ptr);
 
 #endif

8c680df2f4da65864888fa841fb4b02f09116dc2
0xySan <ethtaquet@gmail.com>
2024-10-20 19:05:24 +0200

    did norm

--- pipex/test.c
+++ pipex/test.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 18:56:30 by etaquet           #+#    #+#             */
+/*   Updated: 2024/10/20 19:05:15 by etaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -9,40 +21,34 @@
 #include <string.h>
 #include <sys/wait.h>
 
-char *get_cmd_path(char *arg)
+char	*get_cmd_path(char *arg)
 {
-	char *cpath;
+	char	*cpath;
 
 	cpath = malloc(sizeof(char) * (ft_strlen(CMD_PATH) + ft_strlen(arg) + 1));
 	if (!cpath)
-	{
-		return 0;
-	}
+		return (0);
 	strcpy(cpath, CMD_PATH);
 	strcat(cpath, arg);
-
 	printf("%s", cpath);
 	if (access(cpath, X_OK) == -1)
 	{
 		free(cpath);
 		printf("error cpath");
-		return 0;
+		return (0);
 	}
-	return cpath;
+	return (cpath);
 }
 
-void execute_cmd(char *cmd, char **envp)
+void	execute_cmd(char *cmd, char **envp)
 {
-	char **args;
-	char *cpath;
-       
+	char	**args;
+	char	*cpath;
+
 	args = ft_split(cmd, ' ');
 	cpath = get_cmd_path(args[0]);
-
 	if (!cpath)
-	{
 		return ;
-	}
 	if (execve(cpath, args, envp) == -1)
 	{
 		free(cpath);
@@ -54,19 +60,8 @@ void execute_cmd(char *cmd, char **envp)
 	free(args);
 }
 
-int main(int argc, char **argv, char **envp)
+void	first_child(t_pipex pipex, char **args, char **envp)
 {
-	t_pipex pipex;
-	if (argc != 5)
-		return 0;
-	pipex.infile = open(argv[1], O_RDONLY);
-	if (pipex.infile < 0)
-		return 0;
-	pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
-	if (pipex.outfile < 0)
-		return 0;
-	if (pipe(pipex.tube) == -1)
-		return 0;
 	pipex.pid1 = fork();
 	if (pipex.pid1 == 0)
 	{
@@ -75,8 +70,12 @@ int main(int argc, char **argv, char **envp)
 		close(pipex.tube[0]);
 		close(pipex.infile);
 		close(pipex.outfile);
-		execute_cmd(argv[2], envp);
+		execute_cmd(args[2], envp);
 	}
+}
+
+void	second_child(t_pipex pipex, char **args, char **envp)
+{
 	pipex.pid2 = fork();
 	if (pipex.pid2 == 0)
 	{
@@ -85,13 +84,30 @@ int main(int argc, char **argv, char **envp)
 		close(pipex.tube[1]);
 		close(pipex.infile);
 		close(pipex.outfile);
-		execute_cmd(argv[3], envp);
+		execute_cmd(args[3], envp);
 	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_pipex	pipex;
+
+	if (argc != 5)
+		return (0);
+	pipex.infile = open(argv[1], O_RDONLY);
+	if (pipex.infile < 0)
+		return (0);
+	pipex.outfile = open(argv[argc - 1], O_TRUNC | O_CREAT | O_RDWR, 0000644);
+	if (pipex.outfile < 0)
+		return (0);
+	if (pipe(pipex.tube) == -1)
+		return (0);
+	first_child(pipex, argv, envp);
+	second_child(pipex, argv, envp);
 	close(pipex.tube[0]);
 	close(pipex.tube[1]);
 	close(pipex.infile);
 	close(pipex.outfile);
-
 	waitpid(pipex.pid1, NULL, 0);
 	waitpid(pipex.pid2, NULL, 0);
 }
--- pipex/test.h
+++ pipex/test.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   test.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/20 18:58:14 by etaquet           #+#    #+#             */
+/*   Updated: 2024/10/20 18:59:05 by etaquet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef TEST_H
 # define TEST_H
 
@@ -5,11 +17,11 @@
 
 typedef struct s_pipex
 {
-	pid_t pid1;
-	pid_t pid2;
-	int tube[2];
-	int infile;
-	int outfile;
-} t_pipex;
+	pid_t	pid1;
+	pid_t	pid2;
+	int		tube[2];
+	int		infile;
+	int		outfile;
+}	t_pipex;
 
 #endif

09461e492b0e1a32c7f24676c0fcaf8e0611773d
0xySan <ethtaquet@gmail.com>
2024-10-30 19:44:58 +0100

    finished mandatory part

--- so_long/check_count.c
+++ so_long/check_count.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_count.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: etaquet <etaquet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: etaquet <etaquet@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:00:16 by etaquet           #+#    #+#             */
-/*   Updated: 2024/10/30 17:09:12 by etaquet          ###   ########.fr       */
+/*   Updated: 2024/10/30 19:44:15 by etaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,30 @@ int	ft_count_all(t_init_map *data, int i, int j)
 	if (data->map[i][j] == 'E')
 		return (data->exit++, 0);
 	return (1);
+}
+
+int	ft_check_bordr(t_init_map *data)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (i < data->length)
+		if (data->map[i++][j] != '1')
+			return (1);
+	j++;
+	i = 0;
+	while (j < (data->height - 1))
+	{
+		if(data->map[i][j] != '1' || data->map[data->length - 1][j] != '1')
+			return (1);
+		j++;
+	}
+	while (i < data->length)
+		if (data->map[i++][j] != '1')
+			return (1);
+	return (0);
 }
 
 int	ft_check_count(t_init_map *data)
@@ -47,5 +71,9 @@ int	ft_check_count(t_init_map *data)
 		return (3);
 	if (data->exit != 1)
 		return (4);
+	if (data->length < data->height)
+		return (5);
+	if (ft_check_bordr(data))
+		return (6);
 	return (0);
 }

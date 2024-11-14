37807a8b389237d3c65d4da17b96d1ed83ec2713
0xySan <ethtaquet@gmail.com>
2024-11-14 03:40:44 +0100

    changed return on main file

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: etaquet <etaquet@student.42lehavre.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 15:30:45 by etaquet           #+#    #+#             */
-/*   Updated: 2024/11/14 03:01:46 by etaquet          ###   ########.fr       */
+/*   Updated: 2024/11/14 03:40:11 by etaquet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	check_format_type(char **argv)
 	{
 		free(file_ext);
 		ft_printf("Error\nFormat usage : [map_file.ber] not [%s]\n", argv[1]);
-		exit(EXIT_FAILURE);
+		exit(11);
 	}
 	free(file_ext);
 }
@@ -53,26 +53,23 @@ int	main(int argc, char **argv)
 {
 	t_init_map	so_long;
 
-	if (argc == 2)
-	{
-		check_format_type(argv);
-		ft_map_data(&so_long, argv[1]);
-		ft_read_map(&so_long);
-		ft_check_count(&so_long);
-		ft_map_checker(&so_long);
-		so_long.mlx = mlx_init(so_long.length * 40,
-				(so_long.height + 1) * 40, "So_long", false);
-		ft_parse_map(&so_long);
-		ft_create_map(&so_long);
-		ft_malloc_numbers(&so_long);
-		ft_show_numbers(&so_long);
-		mlx_loop_hook(so_long.mlx, ft_reload_mvmt, &so_long);
-		mlx_loop(so_long.mlx);
-		mlx_terminate(so_long.mlx);
-		ft_free_all(&so_long);
-		printf("Exiting the game.\n");
-		exit(EXIT_SUCCESS);
-	}
-	else
-		ft_printf("Error\nUsage : ./so_long [map_file.ber]\n");
+	if (argc != 2)
+		return (ft_printf("Error\nUsage : ./so_long [map_file.ber]\n"), 10);
+	check_format_type(argv);
+	ft_map_data(&so_long, argv[1]);
+	ft_read_map(&so_long);
+	ft_check_count(&so_long);
+	ft_map_checker(&so_long);
+	so_long.mlx = mlx_init(so_long.length * 40,
+			(so_long.height + 1) * 40, "So_long", false);
+	ft_parse_map(&so_long);
+	ft_create_map(&so_long);
+	ft_malloc_numbers(&so_long);
+	ft_show_numbers(&so_long);
+	mlx_loop_hook(so_long.mlx, ft_reload_mvmt, &so_long);
+	mlx_loop(so_long.mlx);
+	mlx_terminate(so_long.mlx);
+	ft_free_all(&so_long);
+	printf("Exiting the game.\n");
+	return (0);
 }
